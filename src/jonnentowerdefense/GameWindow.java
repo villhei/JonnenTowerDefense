a8e4da284ebb4c1689eff173aa5d8d0ef58264d9
jonne <jonne@ANKKALINNA>
2012-04-14 20:01:30 +0300

    Peli-ikkuna luotu, seuraavaksi ruudukon oikea luonti ja menut.

--- src/jonnentowerdefense/GameWindow.java
+++ src/jonnentowerdefense/GameWindow.java
@@ -11,14 +11,14 @@ import javax.swing.*;
  *
  * @author jonne
  */
-public class GameWindow extends JPanel{
+public class GameWindow {
     
     
     private static char[][] area = {{'B', 'B', 'S', 'B', 'B'},
-                                    {'B', 'B', 'E', 'B', 'B'},
-                                    {'B', 'B', 'E', 'B', 'B'},
-                                    {'B', 'B', 'E', 'B', 'B'},
-                                    {'B', 'B', 'F', 'B', 'B'}};
+                                        {'B', 'B', 'E', 'B', 'B'},
+                                        {'B', 'B', 'E', 'B', 'B'},
+                                        {'B', 'B', 'E', 'B', 'B'},
+                                        {'B', 'B', 'F', 'B', 'B'}};
     JFrame frame;
     private GameArea TDArea;
     final int horSize = 600;
@@ -28,9 +28,7 @@ public class GameWindow extends JPanel{
     public GameWindow(Game game) {
         this.TDGame = game;
         this.TDArea = new GameArea(area);
-        this.AreaArray = TDArea.getArea();
-        horSize = AreaArray.length*60;
-        verSize = AreaArray[0].length*60;
+        
         frame = new JFrame("Jonnen Tower Defense");
         frame.setSize(horSize,verSize);
         frame.setLayout(new BorderLayout());
@@ -83,11 +81,6 @@ public class GameWindow extends JPanel{
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }
-    
-    @Override public void paint(Graphics g) {
-        super.paint(g);
-        
     }
    
 }
